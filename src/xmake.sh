d1a47eba7d85dcf85cbd3a2352414b993f9b8998
ruki <waruqi@gmail.com>
2022-12-16 00:42:01 +0800

    fix configure

--- src/xmake.sh
+++ src/xmake.sh
@@ -38,7 +38,7 @@ target "demo"
     add_headerfiles "${buildir}/include/config.h" "hello"
     add_headerfiles "(bar/*.h)" "hello"
     add_headerfiles "foo/(*.h)" "hello"
-    add_installfiles "res/(png/*.png)" "share"
+    add_installfiles "res/(png/**.png)" "share"
     if has_config "debug"; then
         add_defines "DEBUG" "TEST"
     fi
